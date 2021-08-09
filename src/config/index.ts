ad9e7dc4c8ef157414c43035871ed144e0680e49
pedro-alucard <toxical007@gmail.com>
2021-08-09 20:28:37 -0300

    Atualizando graphql url para o do heroku

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,5 @@ export default {
   url: 'https://strapi-landing-page-project1.herokuapp.com',
   siteName: 'Pedro',
   defaultSlug: 'landing-page',
-  graphqlUrl: 'http://localhost:1337/graphql',
+  graphqlUrl: 'https://blog-nextjs-react.herokuapp.com/graphql',
 };
